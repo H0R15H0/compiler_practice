001878a9ae1bb3b1022fab14fe0a3e97d585b6c6
H0R15H0 <shohori13@gmail.com>
2021-12-02 14:27:25 +0900

    Finish: kadai_02

--- kadai_02/compiler.py
+++ kadai_02/compiler.py
@@ -108,6 +108,173 @@ def p_var_decl_part(p):
                   |
     '''
 
+def p_var_decl_list(p):
+    '''
+    var_decl_list ::= var_decl_list SEMICOLON var_decl
+        | var_decl
+    '''
+
+def p_var_decl(p):
+    '''
+    var_decl ::= VAR id_list
+    '''
+
+def p_subprog_decl_part(p):
+    '''
+    subprog_decl_part ::= subprog_decl_list SEMICOLON
+        | 
+    '''
+
+def p_subprog_decl_list(p):
+    '''
+    subprog_decl_list ::= subprog_decl_list SEMICOLON subprog_decl
+        | subprog_decl
+    '''
+
+def p_subprog_decl(p):
+    '''
+    subprog_decl ::= proc_decl
+    '''
+
+def p_proc_decl(p):
+    '''
+    proc_decl ::= PROCEDURE proc_name SEMICOLON inblock
+    '''
+
+def p_proc_name(p):
+    '''
+    proc_name ::= IDENT
+    '''
+
+def p_inblock(p):
+    '''
+    inblock ::= var_decl_part statement
+    '''
+
+def p_statement_list(p):
+    '''
+    statement_list ::= statement_list SEMICOLON statement
+        | statement
+    '''
+
+def p_statement(p):
+    '''
+    statement ::= assignment_statement
+        | if_statement
+        | while_statement
+        | for_statement
+        | proc_call_statement
+        | null_statement
+        | block_statement
+        | read_statement
+        | write_statement
+    '''
+
+def p_assignment_statement(p):
+    '''
+    assignment_statement ::= IDENT ASSIGN expression
+    '''
+
+def p_if_statement(p):
+    '''
+    if_statement ::= IF condition THEN statement else_statement
+    '''
+
+def p_else_statement(p):
+    '''
+    else_statement ::= ELSE statement
+        | 
+    '''
+
+def p_while_statement(p):
+    '''
+    while_statement ::= WHILE condition DO statement
+    '''
+
+def p_for_statement(p):
+    '''
+    for_statement ::= FOR IDENT ASSIGN expression TO expression DO statement
+    '''
+
+def p_proc_call_statement(p):
+    '''
+    proc_call_statement ::= proc_call_name
+    '''
+
+def p_proc_call_name(p):
+    '''
+    proc_call_name ::= IDENT
+    '''
+
+def p_block_statement(p):
+    '''
+    block_statement ::= BEGIN statement_list END
+    '''
+
+def p_read_statement(p):
+    '''
+    read_statement ::= READ LPAREN IDENT RPAREN
+    '''
+
+def p_write_statement(p):
+    '''
+    write_statement ::= WRITE LPAREN expression RPAREN
+    '''
+
+def p_null_statement(p):
+    '''
+    null_statement ::= 
+    '''
+
+def p_condition(p):
+    '''
+    condition ::= expression EQ expression
+        | expression NEQ expression
+        | expression LT expression
+        | expression LE expression
+        | expression GT expression
+        | expression GE expression
+    '''
+
+def p_expression(p):
+    '''
+    expression ::= term
+        | MINUS term
+        | expression PLUS term
+        | expression MINUS term
+    '''
+
+def p_term(p):
+    '''
+    term ::= f_actor
+        | term MULT f_actor
+        | term DIV f_actor
+    '''
+
+def p_f_actor(p):
+    '''
+    f_actor ::= var_name
+        | number
+        | LPAREN expression RPAREN
+    '''
+
+def p_var_name(p):
+    '''
+    var_name ::= IDENT
+    '''
+
+def p_number(p):
+    '''
+    number ::= NUMBER
+    '''
+
+def p_id_list(p):
+    '''
+    id_list ::= IDENT
+        | id_list COMMA IDENT
+    '''
+
+
 
 #################################################################
 # 構文解析エラー時の処理
@@ -116,7 +283,7 @@ def p_var_decl_part(p):
 def p_error(p):
     if p:
         # p.type, p.value, p.linenoを使ってエラーの処理を書く
-
+        print(f"Syntax error: invalid syntax {p.value} type {p.type} at line {p.lineno}")
     else:
         print("Syntax error at EOF")
 
