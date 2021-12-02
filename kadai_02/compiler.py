9674a48d00899df548bce23b8e83147f06755a35
H0R15H0 <shohori13@gmail.com>
2021-12-02 14:54:25 +0900

    Fix: production token "::=" to ":".

--- kadai_02/compiler.py
+++ kadai_02/compiler.py
@@ -110,56 +110,56 @@ def p_var_decl_part(p):
 
 def p_var_decl_list(p):
     '''
-    var_decl_list ::= var_decl_list SEMICOLON var_decl
+    var_decl_list : var_decl_list SEMICOLON var_decl
         | var_decl
     '''
 
 def p_var_decl(p):
     '''
-    var_decl ::= VAR id_list
+    var_decl : VAR id_list
     '''
 
 def p_subprog_decl_part(p):
     '''
-    subprog_decl_part ::= subprog_decl_list SEMICOLON
+    subprog_decl_part : subprog_decl_list SEMICOLON
         | 
     '''
 
 def p_subprog_decl_list(p):
     '''
-    subprog_decl_list ::= subprog_decl_list SEMICOLON subprog_decl
+    subprog_decl_list : subprog_decl_list SEMICOLON subprog_decl
         | subprog_decl
     '''
 
 def p_subprog_decl(p):
     '''
-    subprog_decl ::= proc_decl
+    subprog_decl : proc_decl
     '''
 
 def p_proc_decl(p):
     '''
-    proc_decl ::= PROCEDURE proc_name SEMICOLON inblock
+    proc_decl : PROCEDURE proc_name SEMICOLON inblock
     '''
 
 def p_proc_name(p):
     '''
-    proc_name ::= IDENT
+    proc_name : IDENT
     '''
 
 def p_inblock(p):
     '''
-    inblock ::= var_decl_part statement
+    inblock : var_decl_part statement
     '''
 
 def p_statement_list(p):
     '''
-    statement_list ::= statement_list SEMICOLON statement
+    statement_list : statement_list SEMICOLON statement
         | statement
     '''
 
 def p_statement(p):
     '''
-    statement ::= assignment_statement
+    statement : assignment_statement
         | if_statement
         | while_statement
         | for_statement
@@ -172,63 +172,63 @@ def p_statement(p):
 
 def p_assignment_statement(p):
     '''
-    assignment_statement ::= IDENT ASSIGN expression
+    assignment_statement : IDENT ASSIGN expression
     '''
 
 def p_if_statement(p):
     '''
-    if_statement ::= IF condition THEN statement else_statement
+    if_statement : IF condition THEN statement else_statement
     '''
 
 def p_else_statement(p):
     '''
-    else_statement ::= ELSE statement
+    else_statement : ELSE statement
         | 
     '''
 
 def p_while_statement(p):
     '''
-    while_statement ::= WHILE condition DO statement
+    while_statement : WHILE condition DO statement
     '''
 
 def p_for_statement(p):
     '''
-    for_statement ::= FOR IDENT ASSIGN expression TO expression DO statement
+    for_statement : FOR IDENT ASSIGN expression TO expression DO statement
     '''
 
 def p_proc_call_statement(p):
     '''
-    proc_call_statement ::= proc_call_name
+    proc_call_statement : proc_call_name
     '''
 
 def p_proc_call_name(p):
     '''
-    proc_call_name ::= IDENT
+    proc_call_name : IDENT
     '''
 
 def p_block_statement(p):
     '''
-    block_statement ::= BEGIN statement_list END
+    block_statement : BEGIN statement_list END
     '''
 
 def p_read_statement(p):
     '''
-    read_statement ::= READ LPAREN IDENT RPAREN
+    read_statement : READ LPAREN IDENT RPAREN
     '''
 
 def p_write_statement(p):
     '''
-    write_statement ::= WRITE LPAREN expression RPAREN
+    write_statement : WRITE LPAREN expression RPAREN
     '''
 
 def p_null_statement(p):
     '''
-    null_statement ::= 
+    null_statement : 
     '''
 
 def p_condition(p):
     '''
-    condition ::= expression EQ expression
+    condition : expression EQ expression
         | expression NEQ expression
         | expression LT expression
         | expression LE expression
@@ -238,7 +238,7 @@ def p_condition(p):
 
 def p_expression(p):
     '''
-    expression ::= term
+    expression : term
         | MINUS term
         | expression PLUS term
         | expression MINUS term
@@ -246,31 +246,31 @@ def p_expression(p):
 
 def p_term(p):
     '''
-    term ::= f_actor
+    term : f_actor
         | term MULT f_actor
         | term DIV f_actor
     '''
 
 def p_f_actor(p):
     '''
-    f_actor ::= var_name
+    f_actor : var_name
         | number
         | LPAREN expression RPAREN
     '''
 
 def p_var_name(p):
     '''
-    var_name ::= IDENT
+    var_name : IDENT
     '''
 
 def p_number(p):
     '''
-    number ::= NUMBER
+    number : NUMBER
     '''
 
 def p_id_list(p):
     '''
-    id_list ::= IDENT
+    id_list : IDENT
         | id_list COMMA IDENT
     '''
 
